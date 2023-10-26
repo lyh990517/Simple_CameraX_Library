4c071417cf336a135ed2f47eb4759a500384d9ce
lyh990517 <lyh990517@naver.com>
2023-10-26 14:56:33 +0900

    사진 찍기 기능 추가

--- app/src/main/java/com/example/camerax/MainActivity.kt
+++ app/src/main/java/com/example/camerax/MainActivity.kt
@@ -1,25 +1,33 @@
 package com.example.camerax
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraControl
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -31,25 +39,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import com.example.camerax.ui.theme.CameraXTheme
-import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 import java.util.concurrent.Executors
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -71,56 +80,71 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FlashControlApp(
     cameraState: State<CameraState>,
     setState: (CameraState) -> Unit
 ) {
-    when (cameraState.value) {
-        is CameraState.PermissionNotGranted -> {
-            RequestPermission(setState)
-        }
+    val snackBarHostState = remember { SnackbarHostState() }
+    Scaffold(topBar = {}, snackbarHost = { SnackbarHost(snackBarHostState) }) {
+        when (cameraState.value) {
+            is CameraState.PermissionNotGranted -> {
+                RequestPermission(setState)
+            }
 
-        is CameraState.Success -> {
-            CameraScreen()
+            is CameraState.Success -> {
+                CameraScreen { message, label, duration ->
+                    CoroutineScope(Dispatchers.Default).launch {
+                        snackBarHostState.showSnackbar(
+                            message, label, duration = duration
+                        )
+                    }
+                }
+            }
         }
     }
 }
 
 @Composable
-private fun CameraScreen() {
+private fun CameraScreen(showSnackBar: (String,String,SnackbarDuration) -> Unit) {
     val flashOn = remember { mutableStateOf(false) }
     val lifecycleOwner = LocalLifecycleOwner.current
     val cameraControl = remember { mutableStateOf<CameraControl?>(null) }
     val cameraProvider by rememberUpdatedState(ProcessCameraProvider.getInstance(LocalContext.current))
-    val coroutineScope = rememberCoroutineScope()
+    val cameraScope = rememberCoroutineScope()
     val context = LocalContext.current
     val preview = remember { mutableStateOf<androidx.camera.core.Preview?>(null) }
     val previewView by remember { mutableStateOf(PreviewView(context)) }
     val cameraFacing = remember { mutableStateOf(CameraSelector.LENS_FACING_BACK) }
     val cameraSelector = CameraSelector.Builder().requireLensFacing(cameraFacing.value).build()
     val provider = cameraProvider.get()
+    val imageCapture = remember { mutableStateOf(ImageCapture.Builder().build()) }
     DisposableEffect(cameraFacing.value) {
-        cameraProvider.addListener(Runnable {
-            coroutineScope.launch(Dispatchers.Main) {
-                createCameraPreview(
+        cameraProvider.addListener({
+            cameraScope.launch(Dispatchers.Main) {
+                startCamera(
                     lifecycleOwner = lifecycleOwner,
                     preview = preview,
                     previewView = previewView,
-                    cameraSelector =  cameraSelector,
+                    cameraSelector = cameraSelector,
                     cameraProvider = provider,
-                    cameraControl = cameraControl
+                    cameraControl = cameraControl,
+                    imageCapture = imageCapture.value
                 )
             }
         }, Executors.newSingleThreadExecutor())
 
         onDispose {
-            cameraControl.value = null
+            provider.unbindAll()
         }
     }
     Box(Modifier.fillMaxSize()) {
-        AndroidView(modifier = Modifier.fillMaxSize(), factory = { previewView }) {}
+        AndroidView(modifier = Modifier.fillMaxSize(), factory = { previewView }) {
+            imageCapture.value = ImageCapture.Builder()
+                .build()
+        }
         Button(
             modifier = Modifier
                 .align(Alignment.BottomStart)
@@ -137,26 +161,38 @@ private fun CameraScreen() {
                 .align(Alignment.BottomCenter)
                 .padding(10.dp),
             onClick = {
-                    if (cameraFacing.value == CameraSelector.LENS_FACING_BACK) {
-                        flashOn.value = false
-                        cameraFacing.value = CameraSelector.LENS_FACING_FRONT
-                    } else {
-                        cameraFacing.value = CameraSelector.LENS_FACING_BACK
-                    }
+                if (cameraFacing.value == CameraSelector.LENS_FACING_BACK) {
+                    flashOn.value = false
+                    cameraFacing.value = CameraSelector.LENS_FACING_FRONT
+                } else {
+                    cameraFacing.value = CameraSelector.LENS_FACING_BACK
+                }
             }
         ) {
             Text(if (cameraFacing.value == CameraSelector.LENS_FACING_FRONT) "back" else "front")
         }
+
+        Button(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(10.dp),
+            onClick = {
+                takePicture(cameraScope,context, imageCapture.value, showSnackBar)
+            }
+        ) {
+            Text("takePicture")
+        }
     }
 }
 
-private fun createCameraPreview(
+private fun startCamera(
     lifecycleOwner: LifecycleOwner,
     preview: MutableState<androidx.camera.core.Preview?>,
     previewView: PreviewView,
     cameraSelector: CameraSelector,
     cameraProvider: ProcessCameraProvider,
-    cameraControl: MutableState<CameraControl?>
+    cameraControl: MutableState<CameraControl?>,
+    imageCapture: ImageCapture
 ) {
     cameraProvider.unbindAll()
 
@@ -168,21 +204,57 @@ private fun createCameraPreview(
     val camera = cameraProvider.bindToLifecycle(
         lifecycleOwner,
         cameraSelector,
-        preview.value
+        preview.value,
+        imageCapture
     )
     cameraControl.value = camera.cameraControl
 }
+fun takePicture(
+    cameraScope: CoroutineScope,
+    context: Context,
+    imageCapture: ImageCapture,
+    showSnackBar: (String, String, SnackbarDuration) -> Unit
+) {
+    val path =
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES + "/cameraX")
+    if (!path.exists()) path.mkdirs();
+    val photoFile = File(
+        path, SimpleDateFormat(
+            "yyyy-MM-dd-HH-mm-ss-SSS", Locale.KOREA
+        ).format(System.currentTimeMillis()) + ".jpg"
+    )
+    val outputFileOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+    cameraScope.launch(Dispatchers.Default + SupervisorJob()) {
+        imageCapture.takePicture(outputFileOptions,
+            ContextCompat.getMainExecutor(context),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(error: ImageCaptureException) {
+                    error.printStackTrace()
+                }
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    showSnackBar(
+                        "Capture Success!! Image Saved at  \n [${Environment.getExternalStorageDirectory().absolutePath}/${Environment.DIRECTORY_PICTURES}/cameraX]",
+                        "close",
+                        SnackbarDuration.Short
+                    )
+                }
+            })
+    }
+}
+
 
 @Composable
 private fun RequestPermission(
     setState: (CameraState) -> Unit
 ) {
     val context = LocalContext.current
-    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()){ granted ->
-        if(granted){
-            setState(CameraState.Success)
+    val launcher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                setState(CameraState.Success)
+            }
         }
-    }
     if (context.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
         LaunchedEffect(Unit) {
             launcher.launch(Manifest.permission.CAMERA)
